69524ae955ebe9f880dee946901ad1062347d057
DESKTOP-9TGI6EJ\psondreicka <patriksamuel2012@gmail.com>
2025-02-05 17:11:27 +0100

    Changed behaviour upon entering site and loging out

--- App/Views/Home/index.view.php
+++ App/Views/Home/index.view.php
@@ -1,15 +1,5 @@
-<div class="container-fluid">
-    <pre>
-        Simple stránka slúžiaca ako semstrálka na VAJKO 🥚
-        Stránka má slúžiť na upload a spracovanie používateľmi vytváraných dát o poečasí
-
-        Na druhý checkpoint tu sú:
-    </pre>
-    <ul>
-        <li>Všetky CRUD operácie pre dáta o počasí (iba na obsah, do úvahy nie sú brané foreign keys modelu
-        (user, location)</li>
-        <li>Kontrola údajov na strane klienta</li>
-        <li>Netriviálny javascript (prevod jednotiek pri zobrazovaní dát)</li>
-        <li>Kontrola údajov na stane servera - tá nemusí byť perfektná z dôvodu konania akcie FRI punč</li>
-    </ul>
-</div>
+<?php
+/** @var \App\Core\LinkGenerator $link */
+header("Location: http://localhost/?c=auth&a=login");
+exit();
+?>
